adbe49b8fc1ca4e53adae0ce35008e430e9bccfa
Robert Rübner <rruebner@bloopark.de>
2016-08-15 17:09:36 +0200

    [IMP] Removed no longer needed functions, updated partner / company synchronisation & updated partner view to work with the use_company_address flag.

--- addons/base_partner_address/models/res_company.py
+++ addons/base_partner_address/models/res_company.py
@@ -26,126 +26,125 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     street = fields.Char(compute='_get_address_data',
-                         inverse='_set_address_data')
+                         inverse='_set_address_street')
     street2 = fields.Char(compute='_get_address_data',
-                          inverse='_set_address_data')
-    zip = fields.Char(compute='_get_address_data', inverse='_set_address_data')
+                          inverse='_set_address_street2')
+    zip = fields.Char(compute='_get_address_data', inverse='_set_address_zip')
     city = fields.Char(compute='_get_address_data',
-                       inverse='_set_address_data')
+                       inverse='_set_address_city')
     state_id = fields.Many2one(compute='_get_address_data',
-                               inverse='_set_address_data')
+                               inverse='_set_address_state_id')
     country_id = fields.Many2one(compute='_get_address_data',
-                                 inverse='_set_address_data')
+                                 inverse='_set_address_country_id')
     email = fields.Char(compute='_get_address_data',
-                        inverse='_set_address_data', store=True)
+                        inverse='_set_email_data', store=True)
     phone = fields.Char(compute='_get_address_data',
-                        inverse='_set_address_data', store=True)
-    fax = fields.Char(compute='_get_address_data', inverse='_set_address_data')
+                        inverse='_set_phone_phone', store=True)
+    fax = fields.Char(compute='_get_address_data', inverse='_set_phone_fax')
 
     @api.depends('partner_id')
     def _get_address_data(self):
         """Read address, phone and email functional fields."""
         for rec in self:
-            street = street2 = zip = city = email = phone = fax = ''
-            state_id = country_id = False
             if rec.partner_id:
                 address = rec.partner_id.preferred_address
                 if address:
-                    street = address.street
-                    street2 = address.street2
-                    zip = address.zip
-                    city = address.city
-                    state_id = address.state_id and \
+                    rec.street = address.street
+                    rec.street2 = address.street2
+                    rec.postcode = address.zip
+                    rec.city = address.city
+                    rec.state_id = address.state_id and \
                         address.state_id.id or False
-                    country_id = address.country_id and \
+                    rec.country_id = address.country_id and \
                         address.country_id.id or False
-                email = rec.partner_id.email
                 phone_rec = rec.partner_id.get_phone('phone')
-                if phone_rec:
-                    phone = phone_rec.name
                 fax_rec = rec.partner_id.get_phone('fax')
-                if fax_rec:
-                    fax = fax_rec.name
+                rec.phone = phone_rec and phone_rec.name or ''
+                rec.fax = fax_rec and fax_rec.name or ''
+                rec.email = rec.partner_id.email
 
-            rec.street = street
-            rec.street2 = street2
-            rec.zip = zip
-            rec.city = city
-            rec.email = email
-            rec.phone = phone
-            rec.fax = fax
-            rec.state_id = state_id
-            rec.country_id = country_id
-
-    def _set_address_data(self):
-        """Write address, phone and email functional fields."""
+    def _set_address_street(self):
         for rec in self:
-            if rec.partner_id:
-                # add / update address data
-                address = rec.partner_id.preferred_address
-                address_data = {
-                    'street': rec.street,
-                    'street2': rec.street2,
-                    'zip': rec.zip,
-                    'city': rec.city,
-                    'state_id': rec.state_id and rec.state_id.id or False,
-                    'country_id': rec.country_id and rec.country_id.id or False
-                }
-                if address:
-                    address.write(address_data)
-                else:
-                    address_data.update({
-                        'name': rec.partner_id.name,
-                        'type': 'business',
-                        'partner_id': rec.partner_id.id
-                    })
-                    self.env['res.partner.address'].create(address_data)
+            rec._set_address_data('street', rec.street)
 
-                # add / update email data
-                email_rec = self.env['res.partner.email'].sudo().search(
-                    [('partner_id', '=', rec.partner_id.id),
-                     ('name', '=', rec.email), ('type', '=', 'business')],
-                    limit=1)
-                email_data = {
-                    'name': rec.email
-                }
-                if email_rec:
-                    email_rec.write(email_data)
-                else:
-                    if rec.partner_id.partner_email_ids:
-                        rec.partner_id.partner_email_ids.write(
-                            {'preferred': False})
-                    email_data.update({
-                        'type': 'business',
-                        'partner_id': rec.partner_id.id,
+    def _set_address_street2(self):
+        for rec in self:
+            rec._set_address_data('street2', rec.street2)
+
+    def _set_address_zip(self):
+        for rec in self:
+            rec._set_address_data('zip', rec.zip)
+
+    def _set_address_city(self):
+        for rec in self:
+            rec._set_address_data('city', rec.city)
+
+    def _set_address_state_id(self):
+        for rec in self:
+            rec._set_address_data('state_id', rec.state_id)
+
+    def _set_address_country_id(self):
+        for rec in self:
+            rec._set_address_data('country_id', rec.country_id)
+
+    def _set_address_data(self, name, value):
+        if name in ['state_id', 'country_id']:
+            value = value.id
+        address = self.partner_id.preferred_address
+        if address:
+            address.write({name: value})
+            if address.is_empty():
+                address.unlink()
+        else:
+            self.env['res.partner.address'].create({
+                'partner_id': self.partner_id.id,
+                'type': 'business',
+                'preferred': True,
+                name: value
+            })
+
+    def _set_phone_phone(self):
+        for rec in self:
+            rec._set_phone_data('phone', rec.phone)
+
+    def _set_phone_fax(self):
+        for rec in self:
+            rec._set_phone_data('fax', rec.fax)
+
+    def _set_phone_data(self, phone_type, value):
+        phone_rec = self.partner_id.get_phone(phone_type)
+        if phone_rec and not value:
+            phone_rec.unlink()
+        elif phone_rec and value:
+            phone_rec.write({'name': value})
+        elif not phone_rec and value:
+            self.env['res.partner.phone'].create({
+                'partner_id': self.partner_id.id,
+                'type': phone_type,
+                'name': value
+            })
+
+    def _set_email_data(self):
+        for rec in self:
+            email = rec.email
+            email_rec = self.env['res.partner.email'].sudo().search(
+                [('partner_id', '=', rec.partner_id.id),
+                 ('type', '=', 'business')])
+            if email_rec and not email:
+                email_rec.unlink()
+                if rec.partner_id.partner_email_ids:
+                    rec.partner_id.partner_email_ids[0].write({
                         'preferred': True
                     })
-                    self.env['res.partner.email'].create(email_data)
-
-                # add / update phone data
-                phone_rec = rec.partner_id.get_phone('phone')
-                phone_data = {
-                    'name': rec.phone
-                }
-                if phone_rec:
-                    phone_rec.write(phone_data)
-                else:
-                    phone_data.update({
-                        'type': 'phone',
-                        'partner_id': rec.partner_id.id
-                    })
-                    self.env['res.partner.phone'].create(phone_data)
-
-                # add / update fax data
-                fax_rec = rec.partner_id.get_phone('fax')
-                fax_data = {
-                    'name': rec.fax
-                }
-                if fax_rec:
-                    fax_rec.write(fax_data)
-                else:
-                    fax_data.update({
-                        'type': 'fax',
-                        'partner_id': rec.partner_id.id
-                    })
-                    self.env['res.partner.phone'].create(fax_data)
+            elif email_rec and email:
+                email_rec.write({'name': email})
+            elif not email_rec and email:
+                if rec.partner_id.partner_email_ids:
+                    rec.partner_id.partner_email_ids.write(
+                        {'preferred': False})
+                self.env['res.partner.email'].create({
+                    'partner_id': rec.partner_id.id,
+                    'type': 'business',
+                    'preferred': True,
+                    'name': email
+                })
